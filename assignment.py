388da9c3f41dc3311a4f16fbaaf25b0457698aab
Tanjim <tanjimahammed776@gmail.com>
2021-02-12 17:48:11 +0600

    assignment.py added

--- assignment.py
+++ assignment.py
@@ -1,11 +1,4 @@
-def oddeven(n1):
-    if n1 % 2 == 0:
-        return'this is an even number'
-    else:
-        return'this is an odd number'
-    
-while True:
-    user = input();    
-    user1 = int(user)
-    call = oddeven(user1)
-    print(call)
+for i in range(1, 21):
+    if i % 5 == 0:
+        print(i)
+        
