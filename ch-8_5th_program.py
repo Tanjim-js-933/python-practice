0b949d378b247851bf5bdce6d577d51a2cf0b14c
Tanjim <tanjimahammed776@gmail.com>
2021-06-08 18:11:55 +0600

    python ask

---
+++ ch-8_5th_program.py
@@ -0,0 +1,3 @@
+str = "This is"
+word = str.count("is")
+print(word)
---
+++ ch-8_6th_program.py
@@ -0,0 +1,11 @@
+import turtle
+name = turtle.textinput("name", "What is your name?")
+name.lower()
+if name.startswith("mr."):
+	print("Hello Sir, how are you?")
+elif name.startswith("mrs") or name.startswith("ms") or name.startswith("ms"):
+	print("Hello Madam, How are you?")
+else:
+	name = name.capitalize()
+	str = "Hi" + name + "! How are you?"
+	print(str)
